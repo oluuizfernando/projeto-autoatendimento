a520a496c59addba45c0051f8c9ac98a0db66f60
oluuizfernando <104778712+oluuizfernando@users.noreply.github.com>
2024-02-14 23:13:11 -0300

    working version using requests

--- downloader.py
+++ downloader.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 import time
+from pathlib import Path
+import requests
 
-def download_pdf(current_user_url):
-    download_dir = "C:\\Users\\eului\\OneDrive\\Documentos\\Documentos\\Repositorios Git\\projeto-autoatendimento\\downloads" # for linux/*nix, download_dir="/usr/Public"
+"""def download_pdf(current_user_url):
+    download_dir = "/downloads" # for linux/*nix, download_dir="/usr/Public"
     options = webdriver.ChromeOptions()
     options.add_experimental_option('prefs', {
     "download.default_directory": download_dir, #Change default directory for downloads
@@ -13,9 +15,19 @@ def download_pdf(current_user_url):
 
     driver = webdriver.Chrome(options=options)
     driver.maximize_window()
-    driver.implicitly_wait(5)
-    #https://transito.mg.gov.br/veiculos/documentos-de-veiculos/imprimir-crlv/exibir_crlve
+    driver.implicitly_wait(3)
+    
     driver.get(current_user_url)
     print('Download feito com sucesso!')
     print(current_user_url)
     time.sleep(10)
+"""
+def download_pdf(current_user_url):
+    filename = Path('downloads/arquivo.pdf')
+    url = current_user_url
+    response = requests.get(url)
+    time.sleep(4)
+    filename.write_bytes(response.content)
+
+if __name__ == '__main__':
+    pass
